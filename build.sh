ab91d75e7cacc309addb6d83ae665eb777d09407
Matthias Fuchs <matthias.fuchs@kps.com>
2017-06-14 20:07:33 +0200

    fixed build script order

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@
 (cd spot-core-model-infrastructure; mvn clean install)
 (cd spot-core-maven-plugins; mvn clean install)
 (cd spot-core-base-models; mvn clean install)
-(cd spot-spring-web-support; mvn clean install)
 (cd spot-core; mvn clean package)
+(cd spot-spring-web-support; mvn clean install)
 (cd spot-mail; mvn clean package)
