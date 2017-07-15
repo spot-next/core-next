5de6f5949e54f21b74c16c184ea29daf3858e2ac
Matthias Fuchs <matthias.fuchs@kps.com>
2017-07-15 17:13:41 +0200

    added first and lastname to customer, added userdetails dto

---
+++ spot-spring-web-support/src/main/java/at/spot/spring/web/dto/UserDetails.java
@@ -0,0 +1,43 @@
+package at.spot.spring.web.dto;
+
+import org.hibernate.validator.constraints.Email;
+
+public class UserDetails {
+	@Email
+	private String email;
+	private String firstName;
+	private String lastName;
+	private String password;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(final String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(final String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(final String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(final String lastName) {
+		this.lastName = lastName;
+	}
+}
