1a6a1eb1815bb3f914d4953a029825257beb0bb7
Matthias Fuchs <meister.fuchs@gmail.com>
2018-07-30 20:44:37 +0200

    item collection proxy factory fixes (fixes persistence IT)

--- spot-core/src/main/java/at/spot/core/infrastructure/support/ItemCollectionFactory.java
+++ spot-core/src/main/java/at/spot/core/infrastructure/support/ItemCollectionFactory.java
@@ -2,27 +2,20 @@ package at.spot.core.infrastructure.support;
 
 import java.lang.reflect.Field;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 import at.spot.core.infrastructure.annotation.Relation;
 import at.spot.core.support.util.ClassUtil;
 import at.spot.core.types.Item;
 
 public class ItemCollectionFactory {
-	private static final Map<Integer, ProxyCollection<?>> proxyCollectionCache = new HashMap<>();
 
 	public static <I extends Item> ProxyCollection<I> wrap(Item owner, String propertyName,
 			Collection<I> collectionToWrap) {
 
-		Field properyField = ClassUtil.getFieldDefinition(owner.getClass(), propertyName, true);
-		ProxyCollection<?> proxyCol = proxyCollectionCache.get(collectionToWrap.hashCode());
-
-		if (proxyCol == null) {
-			proxyCol = new ProxyCollection<I>(collectionToWrap, (e) -> updateOwnerReference(properyField, owner, e),
-					(e) -> updateOwnerReference(properyField, null, e));
-			proxyCollectionCache.put(collectionToWrap.hashCode(), proxyCol);
-		}
+		final Field properyField = ClassUtil.getFieldDefinition(owner.getClass(), propertyName, true);
+		final ProxyCollection<?> proxyCol = new ProxyCollection<I>(collectionToWrap,
+				(e) -> updateOwnerReference(properyField, owner, e),
+				(e) -> updateOwnerReference(properyField, null, e));
 
 		return (ProxyCollection<I>) proxyCol;
 	}
