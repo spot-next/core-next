a314713bac6922f8b67baf8b53997c65a0c9de0e
Matthias Fuchs <meister.fuchs@gmail.com>
2018-08-29 22:33:10 +0200

    cms endpoints fix

---
+++ spot-cms-base/src/main/java/io/spotnext/cms/controller/AbstractPageEndpoint.java
@@ -0,0 +1,19 @@
+package io.spotnext.cms.controller;
+
+import java.util.Map;
+
+import io.spotnext.core.infrastructure.http.ModelAndView;
+import io.spotnext.itemtype.cms.model.CmsPage;
+
+/**
+ * The abstract base class class for HTTP endpoints that will be rendered using
+ * a {@link CmsPage}.
+ */
+public abstract class AbstractPageEndpoint {
+
+	protected ModelAndView renderPage(String layout, String page, Map<String, Object> context) {
+		context.put("page", page);
+
+		return ModelAndView.ok(layout).withPayload(context);
+	}
+}
