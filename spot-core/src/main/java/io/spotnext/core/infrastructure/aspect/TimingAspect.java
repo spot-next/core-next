00b65186e06a7af74bfc2d940bdd612982fbaf2f
Matthias Fuchs <meister.fuchs@gmail.com>
2018-10-10 21:05:43 +0200

    updated jaque, disabled timingaspect

--- spot-core/src/main/java/io/spotnext/core/infrastructure/aspect/TimingAspect.java
+++ spot-core/src/main/java/io/spotnext/core/infrastructure/aspect/TimingAspect.java
@@ -1,12 +1,10 @@
 package io.spotnext.core.infrastructure.aspect;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
 
-@Aspect
+//@Aspect
 public class TimingAspect {
-  @Around("execution(* *(..)) && cflow(execution(* io.spotnext.core.management.service.impl.ModelServiceRestEndpoint.*(..)))")
+//  @Around("execution(* *(..)) && cflow(execution(* io.spotnext.core.management.service.impl.ModelServiceRestEndpoint.*(..)))")
   public Object measureExecutionTime(ProceedingJoinPoint thisJoinPoint) throws Throwable {
     long startTime = System.currentTimeMillis();
     Object result = thisJoinPoint.proceed();
