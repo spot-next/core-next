86791e1246e79e931e5d9f70210705ca1676ef67
Matthias Fuchs <meister.fuchs@gmail.com>
2018-11-02 18:58:54 +0100

    disabled jackson xml test

--- spot-core/src/main/java/io/spotnext/core/infrastructure/strategy/impl/DefaultXmlSerializationStrategy.java
+++ spot-core/src/main/java/io/spotnext/core/infrastructure/strategy/impl/DefaultXmlSerializationStrategy.java
@@ -2,7 +2,6 @@ package io.spotnext.core.infrastructure.strategy.impl;
 
 import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.JacksonXmlModule;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
@@ -23,13 +22,13 @@ public class DefaultXmlSerializationStrategy extends AbstractJacksonSerializatio
 	protected ObjectMapper createMapper() {
 		// enables serialization to XML
 		final JacksonXmlModule xmlModule = new JacksonXmlModule();
-		xmlModule.setDefaultUseWrapper(true);
+//		xmlModule.setDefaultUseWrapper(false);
 		
 		final ObjectMapper objectMapper = new XmlMapper(xmlModule);
 		
 //		objectMapper.configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true);
-		objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
-		objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
+//		objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+//		objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
 
 //		objectMapper.configure(MapperFeature.USE_WRAPPER_NAME_AS_PROPERTY_NAME, true);
 //		objectMapper.configure(MapperFeature.USE_ANNOTATIONS, true);
