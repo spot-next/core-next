8fe7d01fa92f1ff93abc38ac7ac186478aefbf6d
Matthias Fuchs <meister.fuchs@gmail.com>
2018-12-21 21:45:16 +0100

    fixed rest endpoints

--- spot-core/src/main/java/io/spotnext/core/management/service/impl/StatusRestEndpoint.java
+++ spot-core/src/main/java/io/spotnext/core/management/service/impl/StatusRestEndpoint.java
@@ -1,29 +1,22 @@
 package io.spotnext.core.management.service.impl;
 
-import java.io.ByteArrayInputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
+import java.lang.management.ManagementFactory;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.sun.management.OperatingSystemMXBean;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-//import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import io.spotnext.core.infrastructure.annotation.logging.Log;
 import io.spotnext.core.infrastructure.http.DataResponse;
 import io.spotnext.core.infrastructure.http.HttpResponse;
-import io.spotnext.core.infrastructure.service.ImportService;
 import io.spotnext.core.infrastructure.support.LogLevel;
 import io.spotnext.core.infrastructure.support.MimeType;
 import io.spotnext.core.management.annotation.Handler;
 import io.spotnext.core.management.annotation.RemoteEndpoint;
 import io.spotnext.core.management.support.BasicAuthenticationFilter;
 import io.spotnext.core.management.transformer.JsonResponseTransformer;
-import io.spotnext.itemtype.core.beans.ImportConfiguration;
-import io.spotnext.itemtype.core.beans.SerializationConfiguration;
-import io.spotnext.itemtype.core.enumeration.DataFormat;
-import io.spotnext.support.util.ValidationUtil;
 import spark.Request;
 import spark.Response;
 import spark.route.HttpMethod;
@@ -35,73 +28,96 @@ import spark.route.HttpMethod;
  * @version 1.0
  * @since 1.0
  */
-@RemoteEndpoint(portConfigKey = "service.typesystem.rest.port", port = 19000, pathMapping = "/v1/import", authenticationFilter = BasicAuthenticationFilter.class)
+@RemoteEndpoint(portConfigKey = "service.typesystem.rest.port", port = 19000, pathMapping = "/v1/status/", authenticationFilter = BasicAuthenticationFilter.class)
 public class StatusRestEndpoint extends AbstractRestEndpoint {
 
-	@Autowired
-	protected ImportService importService;
+	private ThreadLocal<NumberFormat> formatMegaByte = ThreadLocal.withInitial(() -> new DecimalFormat("0 MB"));
+	private ThreadLocal<NumberFormat> formatPercent = ThreadLocal.withInitial(() -> new DecimalFormat("0%"));
 
-	private static final SerializationConfiguration SERIALIZATION_CONFIG = new SerializationConfiguration();
-	static {
-		SERIALIZATION_CONFIG.setFormat(DataFormat.JSON);
-	}
+	private final OperatingSystemMXBean osMX = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+	private final Runtime runtime = Runtime.getRuntime();
 
 	/**
-	 * Gets all items of the given item type. The page index starts at 1.
+	 * Returns all available status information.
 	 *
 	 * @param          <T> a T object.
 	 * @param request  a {@link spark.Request} object.
 	 * @param response a {@link spark.Response} object.
-	 * @return the response object
+	 * @return the status message
 	 */
 	@Log(logLevel = LogLevel.DEBUG, measureExecutionTime = true)
-	@Handler(method = HttpMethod.post, pathMapping = "", mimeType = MimeType.JSON, responseTransformer = JsonResponseTransformer.class)
-	public <T extends Object> HttpResponse executeImport(final Request request, final Response response) {
-		final String script = request.body();
+	@Handler(pathMapping = "", method = HttpMethod.get, mimeType = MimeType.JSON, responseTransformer = JsonResponseTransformer.class)
+	public <T extends Object> HttpResponse status(final Request request, final Response response) {
+		Map<String, Object> status = new HashMap<>();
+		status.put("cpu", cpuStatus());
+		status.put("memory", memoryStatus());
 
-		if (StringUtils.isNotBlank(script)) {
-			try {
-				final ImportRequest importRequest = serializationService.deserialize(SERIALIZATION_CONFIG, script, ImportRequest.class);
-
-				ValidationUtil.validateNotNull("Import config missing", importRequest.getConfig());
-				ValidationUtil.validateNotEmpty("Import script missing", importRequest.getScript());
-
-				if (StringUtils.isBlank(importRequest.getConfig().getScriptIdentifier())) {
-					importRequest.getConfig().setScriptIdentifier("HTTP request from " + request.ip());
-				}
-
-				importService.importItems(importRequest.getConfig(), new ByteArrayInputStream(importRequest.getScript().getBytes(StandardCharsets.UTF_8)));
-
-				return DataResponse.accepted();
-			} catch (final Exception e) {
-				return handleGenericException(e);
-			}
-		} else {
-			return DataResponse.badRequest().withError("import.nocontent", "Request body must be supplied");
-		}
+		return DataResponse.ok().withPayload(status);
 	}
 
-	public static class ImportRequest {
-		@JsonProperty
-		private ImportConfiguration config;
-		@JsonProperty
-		private String[] script;
-
-		public ImportRequest() {
-			// necessary for deserialization
-		}
-
-		public ImportRequest(ImportConfiguration config, String[] script) {
-			this.config = config;
-			this.script = Arrays.copyOf(script, script.length);
-		}
-
-		public ImportConfiguration getConfig() {
-			return config;
-		}
-
-		public String getScript() {
-			return StringUtils.join(script, "\n");
-		}
+	/**
+	 * Returns information about the currently use memory.
+	 *
+	 * @param          <T> a T object.
+	 * @param request  a {@link spark.Request} object.
+	 * @param response a {@link spark.Response} object.
+	 * @return the status message
+	 */
+	@Log(logLevel = LogLevel.DEBUG, measureExecutionTime = true)
+	@Handler(pathMapping = "/memory", method = HttpMethod.get, mimeType = MimeType.JSON, responseTransformer = JsonResponseTransformer.class)
+	public <T extends Object> HttpResponse memory(final Request request, final Response response) {
+		return DataResponse.ok().withPayload(memoryStatus());
 	}
+
+	/**
+	 * Returns information about the currently use cpu usage.
+	 *
+	 * @param          <T> a T object.
+	 * @param request  a {@link spark.Request} object.
+	 * @param response a {@link spark.Response} object.
+	 * @return the status message
+	 */
+	@Log(logLevel = LogLevel.DEBUG, measureExecutionTime = true)
+	@Handler(pathMapping = "/cpu", method = HttpMethod.get, mimeType = MimeType.JSON, responseTransformer = JsonResponseTransformer.class)
+	public <T extends Object> HttpResponse cpu(final Request request, final Response response) {
+
+		return DataResponse.ok().withPayload(cpuStatus());
+	}
+
+	protected MemoryStatus memoryStatus() {
+		final long freeMemory = runtime.freeMemory();
+		final long maxMemory = runtime.maxMemory();
+		final long allocatedMemory = runtime.totalMemory();
+		final long totalFreeMemory = freeMemory + (maxMemory - allocatedMemory);
+
+		final MemoryStatus status = new MemoryStatus();
+		status.freeMemory = formatAsMB(freeMemory);
+		status.maxMemory = formatAsMB(maxMemory);
+		status.allocatedMemory = formatAsMB(allocatedMemory);
+		status.totalFreeMemory = formatAsMB(totalFreeMemory);
+		return status;
+	}
+
+	protected CpuStatus cpuStatus() {
+		final CpuStatus status = new CpuStatus();
+		status.processCpuLoad = formatPercent.get().format(osMX.getProcessCpuLoad());
+
+		return status;
+	}
+
+	private String formatAsMB(long value) {
+		return formatMegaByte.get().format(value / 1024 / 1024);
+	}
+
+	protected static class CpuStatus {
+		String processCpuLoad;
+	}
+
+	protected static class MemoryStatus {
+		String maxMemory;
+		String allocatedMemory;
+		String freeMemory;
+		String totalFreeMemory;
+	}
+
 }
