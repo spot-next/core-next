c7c12d0b280d87addf4a7b6bee7ee7a4153d8383
Matthias Fuchs <matthias.fuchs@kps.com>
2019-10-13 18:57:28 +0200

    fixed build error

--- spot-core-support/src/main/java/io/spotnext/support/util/MiscUtil.java
+++ spot-core-support/src/main/java/io/spotnext/support/util/MiscUtil.java
@@ -6,6 +6,7 @@ import java.lang.reflect.Array;
 import java.util.Collection;
 import java.util.Locale;
 import java.util.Optional;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
